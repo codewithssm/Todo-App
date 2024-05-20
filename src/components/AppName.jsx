7bc2132f6ef9373b9bab8ef6b1c0b4ad5f8dcdcb
Sahil Malik <sahilmalik0222@gmail.com>
2024-05-20 17:34:34 +0530

    Added new title

--- src/components/AppName.jsx
+++ src/components/AppName.jsx
@@ -1,7 +1,7 @@
 import styles from "./AppName.module.css";
 
 function AppName() {
-  return <h1 className={styles.todoHeading}>TODO LIST</h1>;
+  return <h1 className={styles.todoHeading}>TODO APP</h1>;
 }
 
 export default AppName;
